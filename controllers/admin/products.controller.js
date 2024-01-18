dc5ae7d5591ebdcf65c153bc0a95bea75eec22ee
Duong Kim Nam <duongkimnam2142002@gmail.com>
2024-01-18 15:23:30 +0700

    Lấy ra danh sách sản phẩm

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,5 +1,12 @@
-module.exports.products = (req, res) => {
+const Products = require("../../models/Product");
+
+module.exports.products = async (req, res) => {
+    const products = await Products.find({
+        delete: false, 
+    })
+
     res.render("admin/pages/products/index", {
-        pageTitle: "Trang sản phẩm"
+        pageTitle: "Trang sản phẩm",
+        products: products, 
     })
 }
