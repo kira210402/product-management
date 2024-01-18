c789451eb6ede3769fd6a20f86cf5ff09f271e4e
Duong Kim Nam <duongkimnam2142002@gmail.com>
2024-01-18 16:35:33 +0700

    Thêm tính năng lọc theo trạng thái

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,12 +1,45 @@
 const Products = require("../../models/Product");
 
 module.exports.products = async (req, res) => {
-    const products = await Products.find({
-        delete: false, 
-    })
+    let filterStatus = [
+        {
+            name: "Tất cả",
+            status: "",
+            class: ""
+        },
+        {
+            name: "Hoạt động",
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Dừng hoạt động",
+            status: "inactive",
+            class: ""
+        }
+    ]
+
+    if (req.query.status) {
+        const index = filterStatus.findIndex(item => item.status === req.query.status);
+        filterStatus[index].class = "active";
+    } else {
+        const index = filterStatus.findIndex(item => item.status === "");
+        filterStatus[index].class = "active";
+    }
+
+    let find = {
+        delete: false,
+    }
+
+    if (req.query.status) {
+        find.status = req.query.status;
+    }
+
+    const products = await Products.find(find)
 
     res.render("admin/pages/products/index", {
         pageTitle: "Trang sản phẩm",
-        products: products, 
+        products: products,
+        filterStatus: filterStatus
     })
 }
